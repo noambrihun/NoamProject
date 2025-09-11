063f36819ba3a56aa034a61b7c535689efc25765
noambrihun <noambrihun1@gmail.com>
2025-09-11 14:20:03 +0300

    add something

---
+++ noan.js
@@ -0,0 +1,9 @@
+let arr1 = ['a', 'b'];
+let arr2 = ['x', 'y'];
+const getNewArr = (arr1,arr2) =>{
+return [...arr1,'middle',...arr2];
+};
+console.log(getNewArr(arr1,arr2));
+
+let x = 4;
+console.log(x);
