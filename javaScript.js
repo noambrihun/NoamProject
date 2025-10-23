b1f7a7f81f5d5ec13800917dc6b901854429b7a7
noambrihun <noambrihun1@gmail.com>
2025-10-23 13:59:30 +0300

    first commit

--- javaScript.js
+++ javaScript.js
@@ -3,7 +3,7 @@ let arr2 = ['x', 'y'];
 const getNewArr = (arr1,arr2) =>{
 return [...arr1,'middle',...arr2];
 };
-console.log(getNewArr(arr1,arr2));
+console.log(getNewArr);
 
 let x = 4;
 console.log(x);
